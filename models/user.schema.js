6ef7503c5b50ceae63327f86d00755862735db57
Vimal Kumar <65598747+vimal573@users.noreply.github.com>
2023-01-02 06:10:05 +0000

    aded user model part 1

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,9 +1,35 @@
 import mongoose from "mongoose"
+import AuthRoles from "../utils/authRoles";
 
-const UserSchema = mongoose.Schema({
+const UserSchema = mongoose.Schema(
+    {
     name: {
         type: String,
         required: [true, "Name is required"],
         maxLength: [50, "Name must be less then 50"]
-    }
-})
+    },
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: [8, "Password should be at least 8 characters"],
+        select: false
+    },
+    roles: {
+        enum: Object.values(AuthRoles),
+        default: AuthRoles.USER
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date
+
+},
+{
+    timestamps: true
+}
+);
+
+export default mongoose.model("User", UserSchema);
