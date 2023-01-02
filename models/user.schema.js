1b2361c641fc09cda8851e7166a13409fef41e15
Vimal Kumar <65598747+vimal573@users.noreply.github.com>
2023-01-02 07:04:58 +0000

    hooks for user scjema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose"
 import AuthRoles from "../utils/authRoles";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from 'crypto';
 
 const UserSchema = mongoose.Schema(
     {
@@ -32,5 +35,12 @@ const UserSchema = mongoose.Schema(
     timestamps: true
 }
 );
+
+// encrypt password
+UserSchema.pre("save", async function(next) {
+    if(!this.modified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10);
+    next()
+})
 
 export default mongoose.model("User", UserSchema);
