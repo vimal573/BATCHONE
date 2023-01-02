a6e7de03c1b3a7218914b4025a267e67a04c8a17
Vimal Kumar <65598747+vimal573@users.noreply.github.com>
2023-01-02 12:59:01 +0000

    change modified( ) into isModified( )

--- models/user.schema.js
+++ models/user.schema.js
@@ -39,7 +39,7 @@ const UserSchema = mongoose.Schema(
 
 // encrypt password
 UserSchema.pre("save", async function(next) {
-    if(!this.modified("password")) return next()
+    if(!this.isModified("password")) return next()
     this.password = await bcrypt.hash(this.password, 10);
     next()
 })
