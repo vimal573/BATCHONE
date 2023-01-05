082c6b5b88c730768539b43f788fe3bb64843a2e
Vimal Kumar <65598747+vimal573@users.noreply.github.com>
2023-01-05 07:41:53 +0000

    added morgan and express middleware

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+// morgan loggor
+app.use(morgan("tiny"));
 
 export default app;
