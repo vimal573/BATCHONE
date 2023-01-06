48db313f5f960fc6ca99bb154c42be3a8e8855f4
Vimal Kumar <65598747+vimal573@users.noreply.github.com>
2023-01-06 14:44:18 +0000

    added middleware loggedIn

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,84 +1,86 @@
-import mongoose from "mongoose"
-import AuthRoles from "../utils/authRoles";
-import bcrypt from "bcryptjs";
-import JWT from "jsonwebtoken";
+import mongoose from 'mongoose';
+import AuthRoles from '../utils/authRoles';
+import bcrypt from 'bcryptjs';
+import JWT from 'jsonwebtoken';
 import crypto from 'crypto';
-import Config from "../config";
+import Config from '../config';
 
 const UserSchema = mongoose.Schema(
-    {
+  {
     name: {
-        type: String,
-        required: [true, "Name is required"],
-        maxLength: [50, "Name must be less then 50"]
+      type: String,
+      required: [true, 'Name is required'],
+      maxLength: [50, 'Name must be less then 50'],
     },
     email: {
-        type: String,
-        required: [true, "email is required"],
-        unique: true
+      type: String,
+      required: [true, 'email is required'],
+      unique: true,
     },
     password: {
-        type: String,
-        required: [true, "Password is required"],
-        minLength: [8, "Password should be at least 8 characters"],
-        select: false
+      type: String,
+      required: [true, 'Password is required'],
+      minLength: [8, 'Password should be at least 8 characters'],
+      select: false,
     },
     role: {
-        type: String,
-        enum: Object.values(AuthRoles),
-        default: AuthRoles.USER
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
     },
     forgotPasswordToken: String,
-    forgotPasswordExpiry: Date
-
-},
-{
-    timestamps: true
-}
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
 );
 
 // encrypt password
-UserSchema.pre("save", async function(next) {
-    if(!this.isModified("password")) return next()
-    this.password = await bcrypt.hash(this.password, 10);
-    next()
-})
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 // add more feateures directly to user schema
 
-UserSchema.methods = ({
-    //Compare passward
-    comparePassword: async function(enteredPassword) {
-        return await bcrypt.compare(enteredPassword, this.password)
-    },
+UserSchema.methods = {
+  //Compare passward
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
 
-    //Generate JWT token
-    getJwtToken: function() {
-        return JWT.sign({
-            _id: this._id,
-            role: this.role
-        }),
-        Config.JWT_SECRET,
-        {
-            expiresIn: Config.JWT_EXPIRY
-        }
-    },
+  //Generate JWT token
+  getJwtToken: function () {
+    return (
+      JWT.sign({
+        _id: this._id,
+        role: this.role,
+      }),
+      Config.JWT_SECRET,
+      {
+        expiresIn: Config.JWT_EXPIRY,
+      }
+    );
+  },
 
-    //Generate frogot password token 
+  //Generate frogot password token
 
-    generateForgotPasswordToken: function () {
-        const forgotToken = crypto.randomBytes(20).toString('hex');
+  generateForgotPasswordToken: function () {
+    const forgotToken = crypto.randomBytes(20).toString('hex');
 
-        // step 1 - save to DB
-        this.forgotPasswordToken = crypto.createHash('sha256').update(forgotToken).digest("hex")
+    // step 1 - save to DB
+    this.forgotPasswordToken = crypto
+      .createHash('sha256')
+      .update(forgotToken)
+      .digest('hex');
 
-        this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000
-        // step -2 return values to user
+    this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000;
+    // step -2 return values to user
 
-        return forgotToken
-    }
+    return forgotToken;
+  },
+};
 
-
-})
-
-export default mongoose.model("User", UserSchema);
+export default mongoose.model('User', UserSchema);
