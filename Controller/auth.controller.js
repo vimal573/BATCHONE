ae1c3cb88d4d69c35cfdaf478a8e5472656c789c
Vimal Kumar <65598747+vimal573@users.noreply.github.com>
2023-01-06 05:21:02 +0000

    Auth controller - logIn and LogOut

--- Controller/auth.controller.js
+++ Controller/auth.controller.js
@@ -37,9 +37,70 @@ export const signUp = asyncHandler(async (req, res) => {
 
   res.cookie('token', token, cookieOptions);
 
-  res.statu(200).json({
+  res.status(200).json({
     success: true,
     token,
     user,
+  });
+});
+
+/**********************************************************
+ * @LOGIN
+ * @route http://localhost:4000/api/auth/login
+ * @description User controller for loging user
+ * @parameters email and password
+ * @returns User object
+ **********************************************************/
+
+export const logIn = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError('Please fill all fields', 400);
+  }
+
+  const user = User.findOne({ email }).select('+password');
+
+  if (!user) {
+    throw new CustomError('Invalid credentials', 400);
+  }
+
+  const isPasswordMatched = user.comparePassword;
+
+  if (!isPasswordMatched) {
+    throw new CustomError('Invalid credentials -pass', 400);
+  }
+
+  const token = user.getJwtToken();
+  console.log(user);
+  user.password = undefined;
+
+  res.cookie('token', token, cookieOptions);
+
+  res.status(200).json({
+    success: true,
+    token,
+    user,
+  });
+});
+
+/******************************************************
+ * @LOGOUT
+ * @route http://localhost:5000/api/auth/logout
+ * @description User logout bby clearing user cookies
+ * @parameters
+ * @returns success message
+ ******************************************************/
+
+export const logOut = asyncHandler(async (req, res) => {
+  //   res.clearCookie();
+  res.cookie('token', null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'Logged Out',
   });
 });
